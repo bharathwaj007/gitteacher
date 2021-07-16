601feda9fa14d2394ffb5fb93e6123b50a86da88
bharathwaj007 <bharath1091999@gmail.com>
2021-07-17 00:00:46 +0530

    first commit

---
+++ learn.py
@@ -0,0 +1,29 @@
+class employee:
+    def __init__(self,age,name):
+          self.age=age
+          self.name=name
+          print("super")
+    def details(self):
+        print(self.age)
+        print(self.name)
+
+class Manager(employee):
+    def __init__(self,age,name):
+        super(Manager,self).__init__(age,name)
+        print("lavadi")
+    def printer(self,x,y):
+        print(x+y)
+
+
+
+
+
+
+def main():
+    t=Manager(20,"bharu")
+    t.details()
+    t.printer(20,30)
+    #print("mama")
+
+if __name__=="__main__":
+    main()
